7ed4ee362d9ae1bc87ab394312ecbdbfaf7da40d
liran0666 <ladelshten@gmail.com>
2025-05-14 13:20:01 +0300

    Add files via upload

--- Dom.js
+++ Dom.js
@@ -1,26 +1,31 @@
-let elem1=document.querySelector('#elem1')
-console.log(elem1)
-let elem2= document.querySelector('#elem2');
+let elem1 = document.querySelector('#elem1');
+console.log(elem1);
+let elem2 = document.querySelector('#elem2');
 console.log(elem2);
 let elem3 = document.querySelector('#elem3');
 console.log(elem3);
 
 let elemc = document.querySelector('#elemContent');
-elemc.style.color='red';
+elemc.style.color = 'red';
 
-let jhonson=document.querySelectorAll('.www');
+let jhonson = document.querySelectorAll('.www');
 
-jhonson[1].style.background='red';
-jhonson[0].style.border='1px solid red';
+jhonson[1].style.background = 'red';
+jhonson[0].style.border = '1px solid red';
 
-function changeTitle(){
-  let elem=document.getElementById('mainTitle')
-  elem.textContent="שונה בהצלחה"
-};
+function changeTitle() {
+  let elem = document.getElementById('mainTitle');
+  elem.textContent = 'שונה בהצלחה';
+}
 
-
-
-
+function changecolor() {
+  let colorco = document.getElementById('mainTitle');
+  colorco.style.color = 'blue';
+}
+function poof() {
+  let poof2 = document.getElementById('mainTitle');
+  poof2.style.display = 'none';
+}
 
 //Who the hell is jhonson?!?
 //it is what it is
