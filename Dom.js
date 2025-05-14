cb6bd30b8a1eb213048e7d248ffd4f5125801aee
liran0666 <ladelshten@gmail.com>
2025-05-14 13:01:11 +0300

    Add files via upload

---
+++ Dom.js
@@ -0,0 +1,26 @@
+let elem1=document.querySelector('#elem1')
+console.log(elem1)
+let elem2= document.querySelector('#elem2');
+console.log(elem2);
+let elem3 = document.querySelector('#elem3');
+console.log(elem3);
+
+let elemc = document.querySelector('#elemContent');
+elemc.style.color='red';
+
+let jhonson=document.querySelectorAll('.www');
+
+jhonson[1].style.background='red';
+jhonson[0].style.border='1px solid red';
+
+function changeTitle(){
+  let elem=document.getElementById('mainTitle')
+  elem.textContent="שונה בהצלחה"
+};
+
+
+
+
+
+//Who the hell is jhonson?!?
+//it is what it is
